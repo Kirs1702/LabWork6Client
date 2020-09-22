09d18c35a0a3c3efa5a1b396b1e6451a70d3312e
Kirs1702 <kirilkirs@gmail.com>
2020-09-22 18:31:27 +0300

    Client 22.09.2020

--- src/main/entity/SendReceiver.java
+++ src/main/entity/SendReceiver.java
@@ -280,6 +280,12 @@ public class SendReceiver {
         return new String(b);
     }
 
+    public byte receiveByte() throws IOException {
+        byte[] b = new byte[16];
+        is.read(b);
+        return b[0];
+    }
+
     public void closeConnection() throws IOException {
         gate.close();
     }
